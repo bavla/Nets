c3bef01a12b49958b9f96ed586e17850f7378dd3
bavla <vladimir.batagelj@gmail.com>
2020-02-11 10:26:32 +0100

    Add files via upload

--- source/hist.R
+++ source/hist.R
@@ -1,5 +1,5 @@
 
-biHist <- function(author,TQ,first,last){
+biHist <- function(author,TQ,first,last,PDF=FALSE,w=6,h=4.2){
   i <- 1; k <- 1
   years <- first:last; n <- length(years); value <- rep(0,n) 
   year <- first; v <- 0; TQ <- rbind(TQ,c(last+1,0,0))
@@ -11,9 +11,11 @@ biHist <- function(author,TQ,first,last){
   }
   names <- as.character(years)
   fs <- cumsum(value); fr <- fs-value; D <- rbind(value,fr)
+  if(PDF) pdf(paste(author,".pdf",sep=""),width=w,height=h)
   par(mar=c(6,4,4,4))
   barplot(height=D, col=c("blue","red"), names.arg=names, 
     las=2, cex.axis=0.7, cex.names=0.7, space=0, border = NA 
   )
   text(5,0.9*max(fr),author)
+  if(PDF) dev.off() 
 }
